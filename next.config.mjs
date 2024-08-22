21042eafed533a13758d17a1e81d82c23b4bf761
agreloe <agreloerica@gmail.com>
2024-08-22 12:59:12 +0200

    added pathname to remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
             protocol: 'https',
             hostname: 'shop.agreloe.fun',
             port: '',
+            pathname: '/wp-content/uploads/**',
           },
         ],
       },
